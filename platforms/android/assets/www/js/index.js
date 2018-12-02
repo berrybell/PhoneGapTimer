13f06e08d543f649cea396b106917d3a108c7566
Olga Vorozheykina <solariskind@gmail.com>
2018-12-02 20:13:30 +0200

    chore: Move settings script to separate file

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -228,25 +228,3 @@ exampleTimer.addEventListener("reset", function(e) {
 function showCurrentTime(timer) {
   return timer.getTimeValues().toString();
 }
-
-//Check if vibration is on and show/hide length
-function checkVibrate() {
-  var vibrateOn = $("#vibrate input[type='radio']:checked").val();
-  if (vibrateOn === "true") {
-    $("#vibration-length--wrapper").show();
-  } else $("#vibration-length--wrapper").hide();
-}
-
-// Set vibration length
-function setVibrateLength() {
-  vibroLength = $("#vibration-length").val();
-}
-
-function setBeeps() {
-  beeps = Number($("#beeps").val());
-}
-
-function alertDismissed() {
-  //TODO: do something
-  console.log("Alert dismissed");
-}
